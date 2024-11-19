7c71d49ab9be61ce38f1ad78b380bf2b790ef2f2
Kacian417 <knicho4492@gmail.com>
2024-11-19 02:03:37 +0000

    completed exercise 16

--- .learn/resets/16-Random-Colors-Loop/app.py
+++ .learn/resets/16-Random-Colors-Loop/app.py
@@ -19,15 +19,7 @@ def get_color(color_number=4):
 def get_allStudentColors():
     example_color = get_color(1)
     students_array = []
-    # ✅ ↓ your loop here ↓ ✅ 1.loop 10 times, 2. generate a random number within the loop, 3. call get_color(random_number), 4. push the return random coloe to the student_array
-    for i in range(0, 10):
-        random_number = random.randint(0,3)
-        random_color = get_color(random_number)
-        students_array.append(random_color)
-    
-    return students_array
-
-        
+    # ✅ ↓ your loop here ↓ ✅
     
 
 print(get_allStudentColors())
--- exercises/15-Looping-With-FizzBuzz/app.py
+++ exercises/15-Looping-With-FizzBuzz/app.py
@@ -1,5 +1,15 @@
 def fizz_buzz():
     # ✅↓ Write your code here ↓✅
+    for x in range(0, 101):
+        if (x % 3 == 0 and x % 5 == 0):
+            print("FizzBuzz")
+        elif (x % 3 == 0):
+            print("Fizz")
+        elif (x % 5 == 0):
+            print("Buzz")
+        else:
+            print(x)
+    
 
 # ❌↓ DON'T CHANGE THE CODE BELOW ↓❌
 fizz_buzz()
